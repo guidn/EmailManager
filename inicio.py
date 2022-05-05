8b06297235e293f3d49de2fe95619e8dae869610
guidn <guioliveira852963@gmail.com>
2022-05-05 11:54:55 -0300

    exemplo de lista de email

--- inicio.py
+++ inicio.py
@@ -7,6 +7,16 @@ class Start:
     
     meu_email = MailBox('imap.gmail.com').login(user, senha)
 
+    #LISTA_EMAIL
+    lista_uber = []
+
+    lista_email_uber = meu_email.fetch(AND(from_='uber'))
+
+    print('\nLista de emails do remetente:')
+    for lista in lista_email_uber:
+        lista_uber = {(f'\nEmail ID:{lista.uid}\n{lista.subject}\n{lista.text} {lista.flags} FIMSE')}
+        print(lista_uber)
+
 
 
 Start()
