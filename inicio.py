94d575256ef14e2d8c6daded6aebe7b17ace483b
guidn <guioliveira852963@gmail.com>
2022-05-05 11:51:39 -0300

    logando no imap_tools

--- inicio.py
+++ inicio.py
@@ -2,4 +2,11 @@ from imap_tools import MailBox, AND
 from secreto import Dados
 
 class Start:
+    user = Dados.user()
+    senha = Dados.senha()
     
+    meu_email = MailBox('imap.gmail.com').login(user, senha)
+
+
+
+Start()
